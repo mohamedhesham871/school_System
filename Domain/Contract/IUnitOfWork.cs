acb352cf134b6671181eb5ac95f228ad9b091e34
mohamed Elkady <mmmelkady23@gmail.com>
2025-08-20 01:06:39 +0300

    Adding Unit Of work to my work

---
+++ Domain/Contract/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contract
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChanges();
+        IGenericRepo<TEntity, Tkey> GetRepository<TEntity,Tkey>() where TEntity : class;
+    }
+}
---
+++ Presistance/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Contract;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repository
+{
+    public class UnitOfWork(SchoolDbContexts contexts):IUnitOfWork
+    {
+        private readonly Dictionary<string, object> _Repository = [];
+
+        public IGenericRepo<TEntity, Tkey> GetRepository<TEntity, Tkey>() where TEntity : class
+        {
+            if (_Repository.ContainsKey(typeof(TEntity).Name))
+            {
+                return (IGenericRepo<TEntity, Tkey>)_Repository[typeof(TEntity).Name];
+            }
+            else
+            {     // Create New Object and Add to Dictionry
+                var repo = new GenericRepo<TEntity, Tkey>(contexts);
+                _Repository.Add(typeof(TEntity).Name, repo);
+                return repo;
+            }
+        }
+
+        public Task<int> SaveChanges()
+        {
+          return contexts.SaveChangesAsync();
+        }
+    }
+}
